d83dd062eb61a85e7ad072f8efec467c81b35875
Kishor404 <kishor404@outlook.in>
2025-07-15 14:24:24 +0530

    Contact Us Updated

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -10,13 +10,10 @@ const Footer = () => {
         {/* Contact Us */}
         <div className="footer-column">
           <h3>Contact Us</h3>
-          <p><strong>Dr.S.Rajakarunakaran, Professor & Head (Dept. of Mechanical Engineering)</strong></p>
-          <p><strong>Dr.C.Arunachala Perumal, Professor and Head (Dept. of ECE)</strong></p>
-          <p><strong>Dr.M.Gomathy Nayagam, Professor and Head (Dept. of CSBS)</strong></p>
-          <p><strong>Ramco Institute of Technology</strong></p>
-          <p>North Venganallur, Rajapalayam</p>
-          <p>Virudhunagar - 626117</p>
-          <p>Tamil Nadu, India</p>
+          <p><strong>Dr.S.Erana Veerappa Dinesh, Associate Professor/CSBS</strong><br/>dinesh@ritrjpm.ac.in<br/>Mobile : +91 9843738684</p>
+          <p><strong>Dr.A.Azhagu Jaisudhan Pazhani, Associate Professor/ECE</strong><br/>azhagujaisudhan@ritrjpm.ac.in, <br/>Mobile : +91 9443696072, +91 8524856684</p>
+          <p><strong>Dr.M.Lakshmanan, Associate Professor (S.G)/Mech</strong><br/>lakshmanan@ritrjpm.ac.in<br/>Mobile : +91 7598835844</p>
+          <p><strong>Ramco Institute of Technology</strong><br/>North Venganallur, Rajapalayam<br/>Virudhunagar - 626117<br/>Tamil Nadu, India</p>
         </div>
 
         {/* Quick Menu */}
