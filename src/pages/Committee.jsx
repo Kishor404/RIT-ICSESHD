dc4b2e520b43ec5123a922c9e06b3cfd62d31fb7
Kishor404 <kishor404@outlook.in>
2025-07-15 18:07:18 +0530

    Update The Committee

--- src/pages/Committee.jsx
+++ src/pages/Committee.jsx
@@ -6,6 +6,20 @@ const Committee = () => {
     <section className="committee-section">
       <div className="committee-container">
         
+        <div className="committee-block">
+          <h3>International Advisory Committee</h3>
+          <ul>
+            <li>Prof. Ir. Dr. Yusri Yusof – UTHM, Malaysia</li>
+            <li>Dr. T Samraj Lawrence – Assoc. Prof., IT, Dambi Dollo Univ., Ethiopia</li>
+            <li>Dr. Ben Sujin Bennet – Prof., UTAS, Oman</li>
+            <li>Sreejith Vidhyadharan Nair – Asst. Prof., Aviation, Univ. of North Dakota</li>
+            <li>Dr.Anand ,IIITDM Kottayam</li>
+            <li>Dr.B.Sathish Manikandan,Indian Naval Academy,Ezhimala</li>
+            <li>Dr.Sreejith B,US</li>
+            <li>Dr.Dinesh Antony Raj,Ireland</li>
+            <li>Dr. Ben Sujin Bennet,Professor, - Sultanate of Oman</li>
+          </ul>
+        </div>
 
         <div className="committee-block">
           <h3>National Advisory Committee</h3>
@@ -14,38 +28,25 @@ const Committee = () => {
             <li>Dr. R. Ashok Kumar – Professor, ISE, BMSCE, Bangalore</li>
             <li>Dr. J. Amutharaj Joyson – Dean & Professor, ISE, Raja Rajeswari College, Bangalore</li>
             <li>Dr. Raja S P – Assoc. Prof., School of CSE, VIT Vellore</li>
-            <li>Dr. Mohan K – Asst. Prof. Grade 1, Info Sec., VIT Vellore</li>
-            <li>Dr. Vijayanand S – Asst. Prof., CSE, Jain University, Bangalore</li>
+            <li>Dr. Mohan K – Asst. Prof., Info Sec., VIT Vellore</li>
+            <li>Dr. Vijayanand S – Asst. Prof., Jain University, Bangalore</li>
             <li>Dr. S. Janakiraman – Prof., Dept. of Banking Technology, Pondicherry</li>
             <li>Dr. Shajulin Benedict – Assoc. Prof., IIIT Kottayam</li>
-            <li>Dr. A. Suruliandi – Prof., CSE, MS Univ., Tirunelveli</li>
-            <li>Dr. K. Ramar – Prof., CSE, RMK CET, Chennai</li>
-            <li>Dr. Ganesh Kumar Pugalendhi – Assoc. Prof., CSE, Anna Univ., Chennai</li>
-            <li>Dr. Narendran Rajagopalan – Assoc. Prof., CSE, NIT Puducherry</li>
+            <li>Dr. A. Suruliandi – Prof., MS Univ., Tirunelveli</li>
+            <li>Dr. K. Ramar – Prof., RMK CET, Chennai</li>
+            <li>Dr. Ganesh Kumar Pugalendhi – Assoc. Prof., Anna Univ., Chennai</li>
+            <li>Dr. Narendran Rajagopalan – Assoc. Prof., NIT Puducherry</li>
           </ul>
         </div>
 
-        <div className="committee-block">
-          <h3>International Advisory Committee</h3>
-          <ul>
-            <li>Prof. Ir. Dr. Yusri Yusof – UTHM, Malaysia</li>
-            <li>Dr. T Samraj Lawrence – Assoc. Prof., IT, Dambi Dollo Univ., Ethiopia</li>
-            <li>Dr. Ben Sujin Bennet – Prof., Robotics/AI/IOT, UTAS, Oman</li>
-            <li>Sreejith Vidhyadharan Nair – Asst. Prof., Aviation, Univ. of North Dakota</li>
-            <li>Dr.Anand ,IIITDM Kottayam</li>
-            <li>Dr.B.Sathish Manikandan,Indian Naval Academy,Ezhimala</li>
-            <li>Dr.Sreejith B,US</li>
-            <li>Dr.Dinesh Antony Raj,Ireland</li>
-            <li>Dr.Bensujin</li>
-          </ul>
-        </div>
+        
 
         <div className="committee-block">
           <h3>Industrial Advisory Committee</h3>
           <ul>
             <li>Mr. S. Maniselvam B.E. – Director, Cloudstream Solutions Ltd., UK</li>
             <li>Mr. C. Ganesan – Senior Consultant, Deloitte, Chennai</li>
-            <li>Mr R.Prakash	CDAC,Trivandrum</li>
+            <li>Mr R.Prakash CDAC,Trivandrum</li>
             <li>Mr.Ravindranath ,Scientists</li>
             <li>Mr.Vinoth Kumar,Intel Corporation</li>
             <li>Ms.Danyakaimal</li>
@@ -55,20 +56,48 @@ const Committee = () => {
             <li>Dr.Amuthan,CSE,Pondicherry</li>
             <li>Dr.B.Dhanasekar,Dierctor RC ,Dept of CI,MIT ,Chennai</li>
 
+
           </ul>
         </div>
 
         <div className="committee-block">
           <h3>Internal Advisory Committee</h3>
           <ul>
-            <li>Dr. S. Dharmar – Professor, Civil, RIT</li>
-            <li>Dr. K. Vijayalakshmi – Professor, CSE, RIT</li>
-            <li>Dr. S. Kannan – Professor, EEE, RIT</li>
-            <li>Dr. C. Arunachala Perumal – ECE, RIT</li>
-            <li>Dr. S. Rajakarunakaran – Mechanical, RIT</li>
-            <li>Dr. M. Kaliappan – AI&DS, RIT</li>
-            <li>Dr. M. Gomathy Nayagam – CSBS, RIT</li>
-            <li>Dr. V. Anusuya – IT, RIT</li>
+            <li>Dr. S. Dharmar – Professor & Head, Civil, RIT</li>
+            <li>Dr. K. Vijayalakshmi – Professor & Head , CSE, RIT</li>
+            <li>Dr. S. Kannan – Professor & Head, EEE, RIT</li>
+            <li>Dr. C. Arunachala Perumal – 	Professor & Head ECE, RIT</li>
+            <li>Dr. S. Rajakarunakaran – Professor & Head ,Mechanical, RIT</li>
+            <li>Dr. M. Kaliappan – Professor & Head ,AI&DS, RIT</li>
+            <li>Dr. M. Gomathy Nayagam – Associate Professor & Head ,CSBS, RIT</li>
+            <li>Dr. V. Anusuya – Associate Professor & Head ,IT, RIT</li>
+
+          </ul>
+        </div>
+
+        <div className="committee-block">
+          <h3>Executive Committee</h3>
+          <ul>
+            <li><strong>CHIEF PATRON : </strong>Shri P.R.Venketrama Raja, Chairman, Ramco Group</li>
+            <li><strong>PATRON : </strong>Dr. L. Ganesan, Principal, RIT</li>
+            <li><strong>Co-Patron : </strong>Dr. S. Rajakarunakaran, Vice Principal, RIT</li>
+            <li><strong>Conference Chair : </strong>
+              <ul>
+                <li>Dr. C. Arunachalaperumal, Professor & Head/ECE, RIT</li>
+                <li>Dr.M.GomathyNayagam,Associate Professor & Head/CSBS,RIT</li>
+                <li>Dr.M.Lakshmanan, Associate Professor/Mech,RIT</li>
+              </ul>
+            </li>
+            <li><strong>Conference Treasurer : </strong>Mr.S.ValaiGanesh,Assistant Professor(SG)/Mech,RIT</li>
+            <li><strong>Publication Chair : </strong>
+              <ul>
+                <li>Dr.S.Erana Veerappa Dinesh ,Associate Professor/CSBS,RIT</li>
+                <li>Dr. Dr.J.Jerold John Britto, Assistant Professor (SG)/Mech,RIT</li>
+              </ul>
+            </li>
+            <li><strong>Technical Program Chair : </strong>Dr.A.Azhagu Jaisudhan Pazhani, Associate Professor/ECE,RIT</li>
+            <li><strong>Information Contact : </strong>Mr.A.Ramesh Babu, Assistant Professor II/ECE,RIT</li>
+
           </ul>
         </div>
 
